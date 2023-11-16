1bade791a4356bdb5835fb9b4d548bf815f505ce
muhammadhussain1999 <ksaad7933@gmail.com>
2023-11-16 12:15:38 +0500

    updated dropdwon selector

--- cypress/e2e/CreatePostKeyword.cy.js
+++ cypress/e2e/CreatePostKeyword.cy.js
@@ -7,9 +7,8 @@ describe("Create Project ", () => {
   });
 
   it("Verify that the user is able to create a project", () => {
-
     cy.get('[id="email"]').type("1t.aamer@gmail.com");
-    
+
     cy.get('[id="password"]').type("!Test123*");
 
     cy.get('[class="submit"]').click();
@@ -20,13 +19,16 @@ describe("Create Project ", () => {
       .contains("Artificial Intelligence")
       .click({ force: true });
 
-    cy.get(".project-content-btn.cs-secondary-btn").click({ force: true });
+    cy.get(".project-content-btn .cs-secondary-btn").click({ force: true });
 
     cy.get(".cs-secondary-btn.next-content").click();
 
     cy.get("#new-keyword").type("American History");
 
     cy.get("#new-keyword-context").type("American History description");
+    let dropdwon = "#from-keyword-section div div:nth-child(4)";
+    cy.get(dropdwon).click();
+    cy.get("ul#select2-new-keyword-word-count-results li:nth-child(3)").click();
 
     //  cy.contains('Customize Brief').click()
 
