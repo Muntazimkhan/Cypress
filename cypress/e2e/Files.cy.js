06e8ea174d292fb9ccce535fb212e758efe283e3
muntazim <muntazimk85@gmail.com>
2023-12-04 21:36:54 +0500

    POM Part 1

---
+++ cypress/e2e/Files.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+describe('Create Project ', () => {
+    beforeEach(() =>{
+        cy.visit('https://stagging.contentatscale.ai/login');
+    })
+
+    it('Verify that the user is able to create a project', () => {
+        cy.get('[id="email"]').type('1t.aamer@gmail.com');
+        
+        cy.get('[id="password"]').type('!Test123*');
+
+        cy.get('[class="submit"]').click();
+
+        cy.contains('a', 'Projects').click()
+
+        cy.get('#project_6986').contains('Artificial Intelligence').click({ force: true })
+
+        cy.get('.project-content-btn.cs-secondary-btn').click({ force: true })
+
+        cy.get('.from-file').click({ force: true })
+
+        cy.get('.cs-secondary-btn.next-content').click()
+
+        cy.get('input#content-from-custom-url').type('https://player.fm/series/30758/381599764')
+
+        cy.get('input#new-audio-keyword').type('Talking Cars')
+
+        cy.get('textarea#new-audio-url-context').type('Additional Context')
+
+
+        // cy.contains('Write Post Now').click()
+        
+    });
+
+})
